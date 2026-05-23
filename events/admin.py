864e0d33397689ac4e4474af2053660d7208af63
trishannehui <huidemt@gmail.com>
2026-05-23 13:26:26 +0800

    Delete events directory

--- events/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import EventRegistration
-
-
-admin.site.register(EventRegistration)
-# Register your models here.
--- events/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import register_event, registration_success
-
-
-urlpatterns = [
-    path('', register_event, name='register_event'),
-    path('success/', registration_success, name='registration_success'),
-]
